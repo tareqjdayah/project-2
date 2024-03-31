1d5ee0929ad2b1d24a785fee5617833a5ec35870
tareqjdayah <tareqjdayah22@gmail.com>
2024-04-01 01:26:33 +0300

    build and clean node js project

--- apiRouts/patientRouts.js
+++ apiRouts/patientRouts.js
@@ -16,30 +16,6 @@ router.route("/").get(getAllPatients).post(createPatient);
 router.route("/:patientId").get(getPatientById).put(updatePatient);
 router.route("/:patientId/tests/:testId/fix").post(removeCritical);
 
-// const apicache = require("apicache");
-// const cache = apicache.middleware;
 
-// const {
-//   getpatientByIdValidator,
-//   createPatientValidator,
-//   deletepatientByIdValidator,
-//   updatePatientValidator,
-// } = require("../utils/validator/patientValidator");
-
-// const {
-//   getPatients,
-//   createPatent,
-//   getPatientsById,
-//   deletePatient,
-//   updatePatient,
-// } = require("../service/patientService");
-
-// router.route("/").get(getPatients).post(createPatientValidator, createPatent);
-
-// router
-//   .route("/:id")
-//   .get(getpatientByIdValidator, getPatientsById)
-//   .delete(deletepatientByIdValidator, deletePatient)
-//   .put(updatePatientValidator, updatePatient);
 
 module.exports = router;
